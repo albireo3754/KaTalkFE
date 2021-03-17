ca8facfc04f7218678ef185bea5fc12af18482d9
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-17 20:37:51 +0900

    Tsconfig  webpack.config.js packgage folder structure

---
+++ src/components/Katalk/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Katalk = () => {
+  return <div>왤캐오래걸림? World</div>;
+};
---
+++ src/root.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Katalk } from './components/Katalk';
+
+ReactDom.render(<Katalk />, document.querySelector('#root'));
