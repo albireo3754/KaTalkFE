575b6788e85631c77af5acd594f36cabcd9f559e
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-22 21:00:50 +0900

    Revert "Make User Chat Content"
    This reverts commit cd8336305f48418b81d74544303f62a7936fea45.

--- src/utils/getTime.ts
+++
@@ -1,5 +0,0 @@
-export function getTime(): string {
-  const now = new Date();
-  const sliceEnd = now.toLocaleTimeString().lastIndexOf(':');
-  return now.toLocaleTimeString().slice(0, sliceEnd);
-}
